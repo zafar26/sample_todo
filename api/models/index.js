2b2534ebba09bb1fef92cd3e2a017119be62368c
zafar26 <qutubuddinzafar2616118@gmail.com>
2020-09-28 19:29:10 +0530

    First commit

--- api/models/index.js
+++ api/models/index.js
@@ -7,6 +7,11 @@ const TaskSchema = new Schema({
         type: String,
         required: 'Kindly enter the name of the task'
     }
-});
+},
+    {
+        versionKey: false // set to false then it wont create in mongodb
+
+    }
+);
 
 module.exports = mongoose.model('Tasks', TaskSchema);
